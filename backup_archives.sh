c45f465c81d5cfd8616321e6e9cfb3479d132a0c
Elvin <e.cyubahiro@alustudent.com>
2024-10-14 11:51:46 +0200

    Update backup_archives.sh

--- backup_archives.sh
+++ backup_archives.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Define variables
-GROUP_NUMBER=22  
-ARCHIVE_DIR="archived_logs_group22"
-REMOTE_USER="Elvin100s"  # Replace with your sandbox username
-REMOTE_HOST="031d43926379"  # Replace with your sandbox host
-REMOTE_DIR="/home/hp"  # Directory on the remote server
+GROUP_NUMBER=22  # Replace with your actual group number
+ARCHIVE_DIR="archived_logs_group$GROUP_NUMBER"
+REMOTE_USER="your_username"  # Replace with your sandbox username
+REMOTE_HOST="your_host"       # Replace with your sandbox host
+REMOTE_DIR="/home/"           # Directory on the remote server
 
 # Create the archived_logs directory if it doesn't exist
-mkdir -p archived_logs_group22
+mkdir -p "$archived_logs_group$GROUP_NUMBER"
 
 # Move all archived log files into the archived_logs directory
-mv *archive*.log archived_logs_group22/  
+mv *heart_rate_log.txt_* "$ARCHIVE_DIR/"  
+
 
 # Back up the archived logs to the remote server
-scp -r archived_logs_group$ /* Elvin100sR@031d43926379:archived_logs_group22
+scp -r "$ARCHIVE_DIR/"* "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR"
 
 echo "Backup completed successfully."
+
